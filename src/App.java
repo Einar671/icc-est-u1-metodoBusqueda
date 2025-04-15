5e70b590e7bf9c81f4bf5a495ea9468163d2ecd5
Einar671 <einarkaalhus2809@gmail.com>
2025-04-15 07:33:29 -0500

    creacion de paquetes y clases

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import controllers.MetodoBusqueda;
+
+public class App {
+    private MetodoBusqueda metodoBusqueda = new MetodoBusqueda();
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/controllers/MetodoBusqueda.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import views.ShowConsole;
+
+public class MetodoBusqueda {
+    private ShowConsole sc = new ShowConsole();
+    private int arreglo[];
+
+
+
+    public int busquedaLineal(int arreglo[], int valor){}
+}
