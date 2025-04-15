54b851aa407bf0617660c65a6cd20341582b261f
Einar671 <einarkaalhus2809@gmail.com>
2025-04-15 09:00:10 -0500

    creacion de paquetes y class

--- src/App.java
+++ src/App.java
@@ -1,8 +1,19 @@
 import controllers.MetodoBusqueda;
+import models.Persona;
 
 public class App {
-    private MetodoBusqueda metodoBusqueda = new MetodoBusqueda();
     public static void main(String[] args) {
         
+        Persona[] personas = new Persona[7];
+        personas[0] = new Persona(101,"Juan");
+        personas[1]=new Persona(102, "Maria");
+        personas[2]=new Persona(103,"Carlos");
+        personas[3]=new Persona(104, "Ana");
+        personas[4]=new Persona(105, "Luis");
+        personas[5]=new Persona(106, "Sofia");
+        personas[6]=new Persona(107, "Pedro");
+        
+        MetodoBusqueda metodoBusqueda = new MetodoBusqueda(personas);
+
     }
 }
--- src/controllers/MetodoBusqueda.java
+++ src/controllers/MetodoBusqueda.java
@@ -1,12 +1,56 @@
 package controllers;
 
+import models.Persona;
 import views.ShowConsole;
 
 public class MetodoBusqueda {
-    private ShowConsole sc = new ShowConsole();
-    private int arreglo[];
+    private ShowConsole showConsole;
+    private Persona[] people;
 
+    public MetodoBusqueda() {
 
+    }
 
-    public int busquedaLineal(int arreglo[], int valor){}
+    public MetodoBusqueda(Persona[] persons) {
+
+        showConsole = new ShowConsole();
+        this.people = persons;
+        showPersonByCode();
+    }
+
+    public int busquedaLineal(int arreglo[], int valor) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == valor) {
+                System.out.println("El elemento " + valor + "esta en la pocision: " + i);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int findPersonByCode(int code) {
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].getCode() == code) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void showPersonByCode() {
+        boolean rep=true;
+        while (rep==true) {
+            int codeToFind = showConsole.getCode();
+            int indexPerson = findPersonByCode(codeToFind);
+            if (indexPerson >= 0) {
+                showConsole.showMessage("Persona encontrada");
+                showConsole.showMessage(people[indexPerson].toString());
+                rep=false;
+                break;
+            } else {
+                showConsole.showMessage("Persona con codigo " + codeToFind + " no encontrado");
+            }
+        }
+    }
+
 }
